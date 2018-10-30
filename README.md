13d4b85403fe216dbf5a8ce8a5d8a9f5cec99aab
Tom Clarke <7001196+tomfclarke@users.noreply.github.com>
2018-10-30 17:14:06 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# C++ Library for Adafruit DC + Stepper Motor HAT
+
+C++ port of the official [Adafruit-Motor-HAT-Python-Library](https://github.com/adafruit/Adafruit-Motor-HAT-Python-Library) for use with Raspberry Pi.
+
+Uses the [WiringPi PIN based GPIO access library](http://wiringpi.com/) to control the HAT via I2C.
+
+Written by Tom Clarke. MIT licence.
---
+++ source/i2cdevice.cpp
@@ -0,0 +1,74 @@
+/**
+ *  i2cdevice.cpp
+ *
+ *  MIT License
+ *
+ *  Copyright (c) 2018, Tom Clarke
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+#include "i2c_device.h"
+
+#include <cerrno>
+#include <iostream>
+
+I2CDevice::I2CDevice (int deviceAddress)
+    : address (deviceAddress)
+    , handle (wiringPiI2CSetup (address))
+{
+    if (handle == -1)
+    {
+        std::cerr << "Couldn't open device: ";
+        std::cerr << std::strerror (errno) << std::endl;
+    }
+}
+
+bool I2CDevice::isValid()
+{
+    return handle != -1;
+}
+
+void I2CDevice::write8 (int deviceRegister, int data)
+{
+    if (isValid())
+    {
+        if (wiringPiI2CWriteReg8 (handle, deviceRegister, data) < 0)
+        {
+            std::cerr << "Failed to write to device: ";
+            std::cerr << std::strerror (errno) << std::endl;
+        }
+    }
+}
+
+int I2CDevice::read8 (int deviceRegister)
+{
+    if (isValid())
+    {
+        int data = wiringPiI2CReadReg8 (handle, deviceRegister);
+
+        if (data < 0)
+        {
+            std::cerr << "Failed to read from device: ";
+            std::cerr << std::strerror (errno) << std::endl;
+        }
+
+        return data;
+    }
+}
